0748918b9c0013747f1a1aaefb9f891a65d79fd1
Stephan Bergh <stephiebergh@gmail.com>
2021-08-14 09:57:42 +0200

    Report additions:
    *Added icons for report home
    *Completed TutorDetails Report
    **Fixed routing from tutordetails back to home
    **Generate report and swal alert displays correctly now

--- Learn2CodeAngular/src/app/Report/Report resources/reporting.service.ts
+++ Learn2CodeAngular/src/app/Report/Report resources/reporting.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReportingService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  apiUrl = 'https://localhost:44393/api/Reporting/';
+
+
+    //#region TutorDetails
+    getTutorDetails() {
+      return this.http.get(this.apiUrl + 'TutorDetails');
+
+        //#endregion
+}
 }
--- Learn2CodeAngular/src/app/Report/TutorDetails-Report/tutor-details-report/tutor-details-report.component.ts
+++ Learn2CodeAngular/src/app/Report/TutorDetails-Report/tutor-details-report/tutor-details-report.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import Swal from 'sweetalert2';
-
+import { ReportingService } from '../../Report resources/reporting.service';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tutor-details-report',
@@ -9,10 +12,16 @@ import Swal from 'sweetalert2';
 })
 export class TutorDetailsReportComponent implements OnInit {
 
-  constructor() { }
+tutorList: any = [];
+
+  constructor(
+    private reportService: ReportingService,
+    private router: Router
+  ) { }
 
   
   ngOnInit(): void {
+    this.getTutorDetails()
   }
   x(){
     Swal.fire(
@@ -22,4 +31,31 @@ export class TutorDetailsReportComponent implements OnInit {
     )
   }
 
+  getTutorDetails() {
+    this.reportService.getTutorDetails().subscribe((result) => {
+      this.tutorList = result; 
+      console.log(this.tutorList);
+    });
+  }
+
+  public DownloadPDF():void {
+    let data = document.getElementById('TutorDetailsData');
+      
+    html2canvas(data).then(canvas => {
+        
+        let fileWidth = 240;
+        let fileHeight = canvas.height * fileWidth / canvas.width;
+        
+        const FILEURI = canvas.toDataURL('image/png')
+        let PDF = new jsPDF('p', 'mm', 'a4');
+        let position = 0;
+        PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight)
+        
+        PDF.save('TutorDetailsReport.pdf');
+    });     
+  }
+
+  public RedirectReportHome(){
+    this.router.navigateByUrl('/report-home');
+  }
 }
